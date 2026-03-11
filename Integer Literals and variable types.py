0cfc74266eacdae297e8471d140c90bb93b6e61c
Jerone Kyle Esto <jbesto@paterostechnologicalcollege.edu.ph>
2026-03-12 01:35:23 +0800

    Integer Literals and variable types.py

---
+++ Integer Literals and variable types.py
@@ -0,0 +1,19 @@
+# #2 Integer Literals with Underscores
+
+num1 = 25000000       # without underscores
+num2 = 25_000_000     # with underscores
+
+print("\nInteger Literals:")
+print(num1)
+print(num2)
+
+# #3 Checking Variable Types
+
+num_int = 100
+num_float = 10.5
+num_complex = 2 + 3j
+
+print("\nVariable Types:")
+print("Type of num_int:", type(num_int))
+print("Type of num_float:", type(num_float))
+print("Type of num_complex:", type(num_complex))
